5aad544c315c565d2b7969c75fc8debad2e8cc50
Júlio César Tinin da Silva <jctinin@outlook.com>
2022-07-29 00:16:20 -0300

    pageRequest correction, DTO

--- backend/src/main/java/com/jctinin/crudclient/resources/ClientResources.java
+++ backend/src/main/java/com/jctinin/crudclient/resources/ClientResources.java
@@ -3,13 +3,14 @@ package com.jctinin.crudclient.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jctinin.crudclient.entities.Client;
+import com.jctinin.crudclient.dto.ClientDTO;
 import com.jctinin.crudclient.services.ClientService;
 
 @RestController
@@ -20,16 +21,16 @@ public class ClientResources {
 	private ClientService clientService;
 
 	@GetMapping
-	public ResponseEntity<Page<Client>> findAll(
+	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage",defaultValue = "5") Integer linesPerPage,
-			@RequestParam(value = "name", defaultValue = "orderBy") String name
-			//@RequestParam(value = "direction", defaultValue = "ASC") String direction
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction
 			){
 		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage);
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
-		Page<Client> list = clientService.findAllPaged(pageRequest);
+		Page<ClientDTO> list = clientService.findAllPaged(pageRequest);
 		
 		return ResponseEntity.ok().body(list);
 	}
--- backend/src/main/java/com/jctinin/crudclient/services/ClientService.java
+++ backend/src/main/java/com/jctinin/crudclient/services/ClientService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jctinin.crudclient.dto.ClientDTO;
 import com.jctinin.crudclient.entities.Client;
 import com.jctinin.crudclient.repositories.ClientRepository;
 
@@ -18,11 +19,13 @@ public class ClientService {
 	private ClientRepository clientRepository;
 
 	@Transactional(readOnly = true)
-	public Page<Client> findAllPaged(PageRequest pageRequest) {
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		
 		Page<Client> list = clientRepository.findAll(pageRequest);
+		
+		Page<ClientDTO> result = list.map(element -> new ClientDTO(element));
 				
-		return list;
+		return result;
 	}
 
 }
