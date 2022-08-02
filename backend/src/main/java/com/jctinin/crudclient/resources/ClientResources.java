71750f4b4110e20553477fb68db23690e5d9156d
Júlio César Tinin da Silva <jctinin@outlook.com>
2022-08-02 19:01:38 -0300

    insert e findById

--- backend/src/main/java/com/jctinin/crudclient/resources/ClientResources.java
+++ backend/src/main/java/com/jctinin/crudclient/resources/ClientResources.java
@@ -1,14 +1,20 @@
 package com.jctinin.crudclient.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jctinin.crudclient.dto.ClientDTO;
 import com.jctinin.crudclient.services.ClientService;
@@ -33,5 +39,19 @@ public class ClientResources {
 		Page<ClientDTO> list = clientService.findAllPaged(pageRequest);
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value= "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO client = clientService.findById(id);
+		return ResponseEntity.ok().body(client);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO client){
+		client = clientService.insert(client);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(client.getId()).toUri();
+		return ResponseEntity.created(uri).body(client);
 	}
 }
--- backend/src/main/java/com/jctinin/crudclient/services/ClientService.java
+++ backend/src/main/java/com/jctinin/crudclient/services/ClientService.java
@@ -1,5 +1,6 @@
 package com.jctinin.crudclient.services;
 
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,22 +11,49 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jctinin.crudclient.dto.ClientDTO;
 import com.jctinin.crudclient.entities.Client;
 import com.jctinin.crudclient.repositories.ClientRepository;
+import com.jctinin.crudclient.services.exceptions.ResourceNotFoundException;
 
-
-@Service	
+@Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository clientRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
-		
+
 		Page<Client> list = clientRepository.findAll(pageRequest);
-		
+
 		Page<ClientDTO> result = list.map(element -> new ClientDTO(element));
-				
+
 		return result;
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+
+		Optional<Client> obj = clientRepository.findById(id);
+
+		Client client = obj.orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado."));
+
+		return new ClientDTO(client);
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO insert(ClientDTO clientDto) {
+		Client entity = new Client();
+		this.copyDtoToEntity(clientDto, entity);
+		entity = clientRepository.save(entity);
+		return new ClientDTO(entity);
+
+	}
+
+	private void copyDtoToEntity(ClientDTO clientDto, Client entity) {
+		entity.setName(clientDto.getName());
+		entity.setCpf(clientDto.getCpf());
+		entity.setIncome(clientDto.getIncome());
+		entity.setBirthDate(clientDto.getBirthDate());
+		entity.setChildren(clientDto.getChildren());
 	}
 
 }
