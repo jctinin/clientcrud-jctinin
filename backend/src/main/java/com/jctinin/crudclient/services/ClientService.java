14a3e20210e7fa8fa721470170f466a299091e5d
Júlio César Tinin da Silva <jctinin@outlook.com>
2022-08-02 20:10:51 -0300

    delete

--- backend/src/main/java/com/jctinin/crudclient/services/ClientService.java
+++ backend/src/main/java/com/jctinin/crudclient/services/ClientService.java
@@ -2,7 +2,11 @@ package com.jctinin.crudclient.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jctinin.crudclient.dto.ClientDTO;
 import com.jctinin.crudclient.entities.Client;
 import com.jctinin.crudclient.repositories.ClientRepository;
+import com.jctinin.crudclient.services.exceptions.DatabaseException;
 import com.jctinin.crudclient.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -46,6 +51,31 @@ public class ClientService {
 		entity = clientRepository.save(entity);
 		return new ClientDTO(entity);
 
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO clientDto) {
+
+		try {
+			Client client = clientRepository.getReferenceById(id);
+			this.copyDtoToEntity(clientDto, client);
+			client = clientRepository.save(client);
+			return new ClientDTO(client);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+
+	}
+	
+	public void delete(Long id) {
+		try {
+			clientRepository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new DatabaseException("Id not found " + id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 	private void copyDtoToEntity(ClientDTO clientDto, Client entity) {
